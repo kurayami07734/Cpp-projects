5b2808763e71cb61efd893dab390b28f3d73108c
Aditya Ghidora <adityaghidora@gmail.com>
2022-01-27 22:20:21 +0530

    updated my template file

--- Misc/template.cpp
+++ Misc/template.cpp
@@ -5,10 +5,10 @@
 #include <utility>
 
 #define fastio std::ios_base::sync_with_stdio(false), std::cin.tie(NULL), std::cout.tie(NULL)
-#define vi vector<int>
-#define vd vector<double>
-#define repi(i, f, c) for (int j = i; j < f, j += c)
-#define repd(i, f, c) for (int j = i; j > f; j -= c)
+#define vi std::vector<int>
+#define vd std::vector<double>
+#define repi(a, b, c) for (int i = (int)a; i < (int)b, i += (int)c)
+#define repd(a, b, c) for (int i = (int)a; i > (int)b; i -= (int)c)
 
 using ull = unsigned long long;
 using ll = long long;
--- Scratchpad/src/main.cpp
+++ Scratchpad/src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
 #include "../include/factorial.hpp"
+#define vi std::vector<int>
+#define vd std::vector<double>
+#define repi(i, f, c) for (int j = (int)i; j < (int)f; j += (int)c)
+#define repd(i, f, c) for (int j = (int)i; j > (int)f; j -= (int)c)
 int main()
 {
-	std::cout << "Hello World!\n" << factorial(3);
+	vi v;
+	repi(0, 10, 1)
+	{
+		v.push_back(j);		
+	}
+	repd(v.size()-1,0, 1)
+	{
+		std::cout << v.at(j) << " ";
+	}
 	return 0;
 }
