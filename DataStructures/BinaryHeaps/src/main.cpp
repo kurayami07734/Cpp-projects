d365aaad6302d80e778e68493fbe312523db2669
Aditya Ghidora <adityaghidora@gmail.com>
2022-01-27 17:36:14 +0530

    wrote entry about max binary heap

--- DataStructures/BinaryHeaps/src/main.cpp
+++ DataStructures/BinaryHeaps/src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	MaxBinaryHeap mbh;
-	std::vector<int> v = {1, 2, 3, 5, 4, 6, 10,1020,43,4325,650,21};
+	std::vector<int> v = {1, 2, 3, 5};
 	mbh.insert(v);
 	mbh.display();
 	mbh.extract_max();
