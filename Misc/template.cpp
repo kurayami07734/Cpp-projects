a88a94827be26e72dd08e65bd0d5abfe45be3331
Aditya Ghidora <adityaghidora@gmail.com>
2022-03-11 23:27:28 +0530

    updated the template file

--- Misc/template.cpp
+++ Misc/template.cpp
@@ -10,13 +10,20 @@ using namespace std;
 
 #define fastio std::ios_base::sync_with_stdio(false), std::cin.tie(NULL), std::cout.tie(NULL)
 #define all(x) x.begin(), x.end()
+
+void solve()
+{
+    ll i{0}, j{0}, k{0}, m{0}, n{0}, x{0}, y{0}, z{0};
+}
 int main()
 {
     fastio;
     int T{0};
     cin >> T;
+    // cin >> n;
     while (T--)
     {
+        solve();
     }
     return 0;
 }
